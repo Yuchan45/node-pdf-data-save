033e0d1d62e6bd7b66e94a401be257898c359b4f
Yuchan <yu.nakasone@gmail.com>
2023-05-03 16:45:08 -0300

    Ya le pega al sv y el sv le puede responder bien

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,9 +3,24 @@ const path = require('path');
 
 const mainController = {
     index: async function(req, res) {
-        pdfName = 'g-28_unlocked.pdf';
-        return res.status(200).render('home', { pdfName });    
+        const pdfName = 'g-28_unlocked.pdf';
+        const data = {
+            Example_pdf: {
+                "form1[0].#pageSet[0].Page1[0].PDF417BarCode1[0]": "Tomas",
+                "form1[0].#pageSet[0].Page1[0].PDF417BarCode1[1]": "Yu",
+                "form1[0].#pageSet[0].Page1[0].PDF417BarCode1[2]": "Nakasone",
+            }
+        };
 
+        return res.status(200).render('home', { pdfName, data });    
+    },
+    processData: async function(req, res) {
+        pdfName = 'g-28_unlocked.pdf';
+        const data = {
+            EstoyAca: "asdas"
+        }
+
+        return res.status(200).render('home', { pdfName, data });    
     }
 }
 
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 
-const { index } = mainController;
+const { index, processData } = mainController;
 
 router.get('/', index);
+router.post('/processData', processData);
 
 
 module.exports = router;
