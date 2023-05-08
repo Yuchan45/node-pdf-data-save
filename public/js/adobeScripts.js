3960abb508d145f38b770baacfc1c24480670832
Yuchan <yu.nakasone@gmail.com>
2023-05-08 13:30:49 -0300

    Fixing bug of undefined while logging pdfValues

--- public/js/adobeScripts.js
+++ public/js/adobeScripts.js
@@ -2,9 +2,42 @@ const pdfjsLib = window['pdfjs-dist/build/pdf'];
 
 const responseTxt = document.getElementById("response-text");
 
+
+async function getPDFKeyValues(arrayBuffer) {
+    // Cargar el PDF usando PDF.js
+    pdfjsLib.getDocument({data: arrayBuffer}).promise.then(function(pdf) {
+        let pdfValues = [];
+        const numPaginas = pdf.numPages;
+        // Cargar cada página y extraer los campos de formulario
+
+        for (let i = 1; i <= numPaginas; i++) {
+            pdf.getPage(i).then(function(pagina) {
+                pagina.getAnnotations().then(function(annotations) {
+                    for (let j = 0; j < annotations.length; j++) {
+                        let annotation = annotations[j];
+                        if (annotation.fieldType && annotation.fieldType === 'Tx') {
+                            let fieldName = annotation.fieldName;
+                            let fieldValue = annotation.fieldValue;
+                            // console.log(fieldName + ': ' + fieldValue);
+                            pdfValues.push({fieldName: fieldName, fieldValue: fieldValue});
+                        }
+                    }
+                });
+            });
+        }
+        // console.log("PDF Fields Content: ");
+        console.log("padfValues: ", pdfValues);
+        return pdfValues;
+    });
+
+};
+
+
 document.addEventListener("adobe_dc_view_sdk.ready", function() {
     const pdfName = 'pdfPocosDatosTexts.pdf';
 
+    // const miApiKey = 45fc1d368d724aadb79e26afe3fcbd32;
+    // const ApiKeyMati = 5da6731fa7134ae481916d27d363d44;
     var adobeDCView = new AdobeDC.View({clientId: "45fc1d368d724aadb79e26afe3fcbd32", divId: "adobe-dc-view"});
     adobeDCView.previewFile({
         content: {location: {url: "/outputPdfs/" + pdfName}},
@@ -34,67 +67,37 @@ document.addEventListener("adobe_dc_view_sdk.ready", function() {
         // Este metodo se ejecuta cuando se guarda el pdf.
         AdobeDC.View.Enum.CallbackType.SAVE_API,
 
-        function(metaData, content, options) {
+        async function(metaData, content, options) {
             /* Add your custom save implementation here...and based on that resolve or reject response in given format */
             const pdfArrayBuffer = content;
-            // console.log("Content del pdf" + pdfArrayBuffer);
+            // fs.writeFile('rotateTest.pdf', arrayBuffer);
 
-            const arrayBuffer = pdfArrayBuffer;
+            const results = await getPDFKeyValues(pdfArrayBuffer);
+            console.log("results: ", results);
+            
+            // const data2 = [
+            //     { name: 'John', age: 30 },
+            //     { name: 'Jane', age: 25 },
+            //     { name: 'Bob', age: 40 }
+            //   ];
+            // console.log(data2)
+            // Send to sv.
+            // const url = '/processData';
 
-            // Cargar el PDF usando PDF.js
-            pdfjsLib.getDocument({data: arrayBuffer}).promise.then(function(pdf) {
-                // Obtener el número de páginas del PDF
-                const numPaginas = pdf.numPages;
-                // console.log(numPaginas);
-                // Cargar cada página y extraer los campos de formulario
-                let pdfValues = [];
+            // const options = {
+            //     headers: {
+            //       'Content-Type': 'application/json'
+            //     }
+            // };
 
-                for (let i = 1; i <= numPaginas; i++) {
-                    pdf.getPage(i).then(function(pagina) {
-                        pagina.getAnnotations().then(function(annotations) {
-                            for (let j = 0; j < annotations.length; j++) {
-                                let annotation = annotations[j];
-                                if (annotation.fieldType && annotation.fieldType === 'Tx') {
-                                    let fieldName = annotation.fieldName;
-                                    let fieldValue = annotation.fieldValue;
-
-                                    // Hacer algo con el nombre y el valor del campo de formulario
-                                    // console.log(fieldName + ': ' + fieldValue);
-                                    pdfValues.push({fieldName: fieldName, fieldValue: fieldValue});
-                                }
-                            }
-                        });
-                    });
-                }
-                console.log("PDF Fields Content: ");
-                console.log(pdfValues);
-
-
-                // const data2 = [
-                //     { name: 'John', age: 30 },
-                //     { name: 'Jane', age: 25 },
-                //     { name: 'Bob', age: 40 }
-                //   ];
-                // console.log(data2)
-                // Send to sv.
-                // const url = '/processData';
-
-                // const options = {
-                //     headers: {
-                //       'Content-Type': 'application/json'
-                //     }
-                // };
-
-                // // Enviar la petición axios
-                // axios.post(url, data2, options)
-                //     .then(response => {
-                //         console.log(response.data2);
-                //     })
-                //     .catch(error => {
-                //         console.error('Error:', error);
-                //     });
-
-            });
+            // // Enviar la petición axios
+            // axios.post(url, data2, options)
+            //     .then(response => {
+            //         console.log(response.data2);
+            //     })
+            //     .catch(error => {
+            //         console.error('Error:', error);
+            //     });
 
 
             return new Promise((resolve, reject) => {
